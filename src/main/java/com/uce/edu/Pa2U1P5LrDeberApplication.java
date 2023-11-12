175127cec58279c291bed92475fc9295780320bf
Leonardo Ramirez <ldramirez@uce.edu.ec>
2023-11-11 19:16:53 -0600

    subir proyecto por primera ver

---
+++ src/main/java/com/uce/edu/Pa2U1P5LrDeberApplication.java
@@ -0,0 +1,22 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class Pa2U1P5LrDeberApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5LrDeberApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Programación Avanzada 2");
+		System.out.println("Deber 2, uso de GitHub");
+		System.out.println("Leonardo Ramirez");
+	}
+
+}
