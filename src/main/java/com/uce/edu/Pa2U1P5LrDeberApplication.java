0e54274e4d7e66b6e60182286f1fe119a232824b
LeonardoDavidRY <ldramirez@uce.edu.ec>
2025-12-03 18:26:26 -0500

    prueba commit

--- src/main/java/com/uce/edu/Pa2U1P5LrDeberApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5LrDeberApplication.java
@@ -19,6 +19,8 @@ public class Pa2U1P5LrDeberApplication implements CommandLineRunner {
 		System.out.println("Leonardo Ramirez");
 		System.out.println("Cambio primer commit");
 		System.out.println("Probando");
+		// nuevo cambio en el commit
+		
 		
 	}
 
